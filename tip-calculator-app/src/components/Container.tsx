8072571cd0c1671b91137bee94f746affd0cdd1b
K1dou <hique1276@gmail.com>
2025-04-26 21:11:28 -0300

    feat: integrate Tailwind CSS and update project structure
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Removed App.css and migrated styles to Tailwind CSS.
    - Created a new Container component for layout purposes.
    - Updated App component to use Tailwind CSS for styling.
    - Added new SVG icons and a preview image to the public directory.
    - Created a style guide for front-end design specifications.
    - Updated main.tsx to wrap App component with Container.

---
+++ tip-calculator-app/src/components/Container.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react"
+
+interface ContainerPros {
+    children: ReactNode,
+    className?: string;
+
+}
+
+export default function Container({ children, className }: ContainerPros) {
+    return (
+        <div className={` md:flex md:items-center md:justify-center w-full bg-Navy-950  max-w-screen-md md:max-w-screen-lg lg:max-w-full mx-auto px-6  md:px-12 lg:px-20 ${className}`}>
+            {children}
+        </div>
+    )
+}
--- tip-calculator-app/src/main.tsx
+++ tip-calculator-app/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
+import Container from './components/Container'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Container> <App /></Container>
+
   </StrictMode>,
 )
