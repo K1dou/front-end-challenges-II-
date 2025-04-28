a86adfa625f3c675bf18d6478f42c563d58af15e
K1dou <hique1276@gmail.com>
2025-04-28 11:44:03 -0300

    started ip-address-tracker

---
+++ ip-address-tracker/src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-blue-500'>teste</h1>
+    </>
+  )
+}
+
+export default App
---
+++ ip-address-tracker/src/components/Container.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react"
+
+interface ContainerPros {
+    children: ReactNode,
+    className?: string;
+
+}
+
+export default function Container({ children, className }: ContainerPros) {
+    return (
+        <div className={`flex justify-center min-h-screen w-full max-w-screen-md md:max-w-screen-lg lg:max-w-full mx-auto px-6  md:px-12 lg:px-20 ${className}`}>
+            {children}
+        </div>
+    )
+}
