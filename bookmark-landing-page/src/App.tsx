37c900c5fc0c4d21a1cd98a5b18b909d9107a024
K1dou <hique1276@gmail.com>
2025-04-29 23:56:11 -0300

    fix

--- bookmark-landing-page/src/App.tsx
+++ bookmark-landing-page/src/App.tsx
@@ -1,11 +1,9 @@
 import AskedQuestion from './components/AskedQuestions';
 import Browsers from './components/Browsers';
-import Container from './components/Container';
 import Features from './components/Features';
 import Footer from './components/Footer';
 import FormEmail from './components/FormEmail';
 import SessionTopo from './components/SessionTopo';
-import TabsMain from './components/TabsMain';
 import './index.css';
 function App() {
 
