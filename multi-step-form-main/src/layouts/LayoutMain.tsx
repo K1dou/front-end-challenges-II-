0e58422742cb720d917856e9d029316d0a0b1862
K1dou <hique1276@gmail.com>
2025-05-01 22:51:56 -0300

    fix

--- multi-step-form-main/src/layouts/LayoutMain.tsx
+++ multi-step-form-main/src/layouts/LayoutMain.tsx
@@ -7,9 +7,9 @@ export default function LayoutMain({ children }: { children: React.ReactNode }) 
     return (
 
 
-        <div className="min-h-[95vh] flex flex-col">
+        <div className="min-h-[100dvh] flex flex-col">
             <NavMain />
-            <main className="flex-1 flex flex-col">
+            <main className="flex-1 flex flex-col ">
                 {children}
             </main>
 
